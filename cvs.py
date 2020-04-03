4817c90aac42104af943337a8457a4708a3c4bf2
Francesco De Carlo <decarlof@gmai.com>
2020-04-03 16:55:18 -0500

    first commit

--- cvs.py
+++ cvs.py
@@ -1,7 +1,7 @@
 
 import pandas
 
-data = pandas.read_csv('/Users/decarlo/conda/readers/data.cvs', sep = "\s+|\t+|\s+\t+|\t+\s+")
+data = pandas.read_csv('/Users/decarlo/conda/readers/data.cvs', sep = "\s+|\t+|\s+\t+|\t+\s+", engine='python')
 
 print(data.head())
 print(data)
