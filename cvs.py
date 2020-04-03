4987be1141808981ee56b341be97e9087079a177
Francesco De Carlo <decarlof@gmai.com>
2020-04-03 16:56:26 -0500

    first commit

--- cvs.py
+++ cvs.py
@@ -1,7 +1,7 @@
+import matplotlib.pyplot as plt
+import pandas as pd
 
-import pandas
-
-data = pandas.read_csv('/Users/decarlo/conda/readers/data.cvs', sep = "\s+|\t+|\s+\t+|\t+\s+", engine='python')
+data = pd.read_csv('/Users/decarlo/conda/readers/data.cvs', sep = "\s+|\t+|\s+\t+|\t+\s+", engine='python')
 
 print(data.head())
 print(data)
